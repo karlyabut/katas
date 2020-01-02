0358248487a8ad617ed9ca99e7ff493f5cc18c5c
Karl Eisen Yabut <karleisenyabut@gmail.com>
2020-01-02 00:28:26 -0500

    valid parentheses

---
+++ codewars/validParentheses.js
@@ -0,0 +1,20 @@
+function validParentheses(parens){
+  //TODO 
+  let validateCount = 0;
+
+  for(let x of parens) {
+    if(x === "(") {
+      validateCount += 1
+    } else if(x === ")") {
+      if(validateCount === 0) {
+        return false
+      } else {
+        validateCount--
+      }
+    }
+  }
+  if(validateCount === 0) {
+    return true;
+  }
+  return false
+}
