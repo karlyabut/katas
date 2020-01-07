8a08d353c316be76d1c79de8370087655b1081a3
Karl Eisen Yabut <karleisenyabut@gmail.com>
2020-01-06 19:47:34 -0500

    kata: phone number formatting

---
+++ codewars/createPhoneNumber.js
@@ -0,0 +1,7 @@
+function createPhoneNumber(numbers){
+  const numberStr = numbers.join('');
+  const phoneNum = numberStr.match(/^(\d{3})(\d{3})(\d{4})$/)
+  return `(${phoneNum[1]}) ${phoneNum[2]}-${phoneNum[3]}`
+}
+
+//createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) => returns "(123) 456-7890"
