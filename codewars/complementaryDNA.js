3337bf03bbaca882d8abebbe6f9131be9ee54432
Karl Eisen Yabut <karleisenyabut@gmail.com>
2020-02-04 22:52:33 -0500

    kata: complementary DNA

---
+++ codewars/complementaryDNA.js
@@ -0,0 +1,19 @@
+function DNAStrand(dna){
+  const finalArray = [];
+  for(let str of dna) {
+    if(str === 'A') {
+      finalArray.push('T')
+    } else if (str === 'T') {
+      finalArray.push('A')
+    } else if (str === 'C') {
+      finalArray.push('G')
+    } else if (str === 'G') {
+      finalArray.push('C')
+    }
+  }
+  return finalArray.join('');
+}
+
+console.log(DNAStrand ("ATTGC")); // return "TAACG"
+
+console.log(DNAStrand ("GTAT")); // return "CATA" 
